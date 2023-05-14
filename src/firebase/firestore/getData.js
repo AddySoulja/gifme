dbd503c54f6bfbd16dd5fbc54a7f8ea263ba1be9
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-14 23:09:21 +0530

    firebase debug

--- src/firebase/firestore/getData.js
+++ src/firebase/firestore/getData.js
@@ -1,23 +1,14 @@
-import { db } from "../firebaseConfig";
+// import { db } from "../firebaseConfig";
 
-export default async function getDoument(collection) {
-  const favRefs = db.collection(collection);
-
+export default async function getFavorites(collection) {
   let result = null;
   let error = null;
 
   try {
-    result = await favRefs.get();
+    // result = await favRefs.get();
   } catch (e) {
     error = e;
   }
 
   return { result, error };
-}
-const cityRef = db.collection("cities").doc("SF");
-const doc = await cityRef.get();
-if (!doc.exists) {
-  console.log("No such document!");
-} else {
-  console.log("Document data:", doc.data());
 }
