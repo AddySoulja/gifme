e939bf8668f1a755b1c4ad71c412f6c3f240fa7a
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-14 23:26:56 +0530

    Folder structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,23 +3,12 @@ import React, { useState } from "react";
 import Search from "./search/Search";
 import PopulateGifs from "./populate/PopulateGifs";
 import Pagination from "./pagination/Pagination";
-import Link from "next/link";
-import logOut from "../../src/auth/LogOut";
 
-export default function Dashboard({ user }: { user: { displayName: string } }) {
+export default function Dashboard() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   return (<>
-    <div className="navbar">
-      <span className="email">{user !== null ? user.displayName : ""}</span>
-      <Link href="/favorites" className="navBtn">
-        Favorites
-      </Link>
-      <button type="button" className="navBtn" onClick={() => logOut()}>
-        Log out
-      </button>
-    </div>
     <div className="searchbar">
       <Search setData={setData} setIsLoading={setIsLoading} offset={page * 3} />
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Dashboard from "./dashboard/page";
 import { getAuth } from "firebase/auth";
 import app from "@/src/firebase/firebaseConfig";
-
+import Link from "next/link";
+import logOut from "../src/auth/LogOut";
 export default function Home() {
   const user = getAuth(app).currentUser;
+  const [displayName, setDisplayName] = useState("");
   const router = useRouter();
+
   useEffect(() => {
     if (user === null) return router.push("/log-in")
-  }, [user, router])
+    setDisplayName(() => user.displayName === null ? "" : user.displayName);
+  }, [user, router]);
 
   return (
     <main className="main">
-      <Dashboard user={user} />
+      <div className="navbar">
+        <span className="email">{displayName !== "" ? displayName : ""}</span>
+        <Link href="/favorites" className="navBtn">
+          Favorites
+        </Link>
+        <button type="button" className="navBtn" onClick={() => logOut()}>
+          Log out
+        </button>
+      </div>
+      <Dashboard />
     </main>
   )
 }
