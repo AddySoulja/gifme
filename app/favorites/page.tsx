7557b95e7fec2addf50a0577c65d605ba1686492
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-15 00:53:32 +0530

    Log out feature

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Favorites() {
     return (<><div className="main">
         <div className="fav">
             <h1 className="fav-heading">My favorites</h1>
+            <Link href="/" className="navBtn" id="homeBtn">Home</Link>
         </div>
     </div></>)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Dashboard from "./dashboard/page";
 import { getAuth } from "firebase/auth";
+import Dashboard from "./dashboard/page";
 import app from "@/src/firebase/firebaseConfig";
 import Link from "next/link";
 import logOut from "../src/auth/LogOut";
+
 export default function Home() {
-  const user = getAuth(app).currentUser;
+  const auth = getAuth(app);
+  const user = auth.currentUser;
   const [displayName, setDisplayName] = useState("");
   const router = useRouter();
 
+  const handleLogOut = useCallback(async () => {
+    try {
+      await logOut();
+    } catch (e) {
+      alert(e);
+    }
+    return router.push("/log-in");
+  }, [router])
+
   useEffect(() => {
-    if (user === null) return router.push("/log-in")
+    if (user === null) return router.push("/log-in");
     setDisplayName(() => user.displayName === null ? "" : user.displayName);
-  }, [user, router]);
+  }, [user, router, auth]);
 
   return (
     <main className="main">
@@ -24,7 +35,7 @@ export default function Home() {
         <Link href="/favorites" className="navBtn">
           Favorites
         </Link>
-        <button type="button" className="navBtn" onClick={() => logOut()}>
+        <button type="button" className="navBtn" onClick={handleLogOut}>
           Log out
         </button>
       </div>
