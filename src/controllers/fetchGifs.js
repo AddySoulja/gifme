41c61c707175ec18a402114fc9ad05bb5002594c
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-14 23:49:40 +0530

    SSR fetch

--- src/controllers/fetchGifs.js
+++ src/controllers/fetchGifs.js
@@ -3,7 +3,10 @@ const api_key = "GlVGYHkr3WSBnllca54iNt0yFbjz7L65";
 
 export async function fetchGifs(keyword, offset) {
   const res = await fetch(
-    `${url}q=${keyword}&api_key=${api_key}&limit=3&offset=${offset}`
+    `${url}q=${keyword}&api_key=${api_key}&limit=3&offset=${offset}`,
+    {
+      cache: "no-store",
+    }
   );
   return res.json();
 }
